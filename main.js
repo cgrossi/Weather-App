b954cb3505322eb0bc2c78c86b231313162639f1
Carmine <cgrossi@gmail.com>
2019-05-23 15:09:33 -0600

    Mixed content error

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ window.addEventListener('load', () => {
                         lat = position.coords.latitude;
 
                         const proxy = 'http://cors-anywhere.herokuapp.com/';
-                        const api = `${proxy}https://api.darksky.net/forecast/07adfcca545054047deac0a9d034860d/${lat},${long}`;
+                        const api = `https://api.darksky.net/forecast/07adfcca545054047deac0a9d034860d/${lat},${long}`;
 
                         fetch(api)
                                 .then(response => response.json())
