82a12632a3e234230d7c80c1b12f4db5776f8b09
Carmine <cgrossi@gmail.com>
2019-05-23 15:02:54 -0600

    Add Functionality

--- main.js
+++ main.js
@@ -1,1 +1,81 @@
-console.log('Connected');
+window.addEventListener('load', () => {
+        let long;
+        let lat;
+
+        // DOM Elements
+        const timezone = document.querySelector('.location-name');
+        const temp = document.querySelector('.temperature-degree');
+        const tempDesc = document.querySelector('.temperature-description');
+        const degreeSection = document.querySelector('.degree-section');
+        const degreeUnit = document.querySelector('.temperature-degree-unit');
+        const icon = document.querySelector('.location-icon');
+
+        // Convert temp functions
+        const convertToCelsius = tempF => {
+                if (typeof tempF === 'string') {
+                        return Math.round((+tempF - 32) * (5 / 9));
+                }
+                return Math.round((tempF - 32) * (5 / 9));
+        };
+
+        const convertToFahrenheit = tempC => {
+                if (typeof tempC === 'string') {
+                        return Math.round(+tempC * (9 / 5) + 32);
+                }
+                return Math.round(tempC * (9 / 5) + 32);
+        };
+
+        // API Call
+        if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(position => {
+                        long = position.coords.longitude;
+                        lat = position.coords.latitude;
+
+                        const proxy = 'http://cors-anywhere.herokuapp.com/';
+                        const api = `${proxy}https://api.darksky.net/forecast/07adfcca545054047deac0a9d034860d/${lat},${long}`;
+
+                        fetch(api)
+                                .then(response => response.json())
+                                .then(data => {
+                                        console.log(data);
+                                        tempDesc.textContent = `${data.daily.summary}`;
+                                        temp.textContent = `${Math.round(data.currently.temperature)}`;
+                                        timezone.textContent = `${data.timezone}`;
+                                        icon.textContent = `${data.currently.icon}`;
+                                });
+                });
+        } else {
+                alert(
+                        `This weather app needs your current location to get your weather data. Please refresh and click on 'allow'. Otherwise, no weather for you.`
+                );
+        }
+
+        // Convert temp event
+        degreeSection.addEventListener('click', () => {
+                if (degreeUnit.textContent === 'F') {
+                        const tempC = convertToCelsius(temp.textContent);
+                        degreeUnit.textContent = 'C';
+                        temp.textContent = tempC;
+
+                        // ----Not Working Yet. Search weather description for temp and replace with converted ------//
+                        // const tempDescTemp = tempDesc.textContent.match(/[\d]+/)[0];
+                        // tempDesc.textContent = tempDesc.textContent.replace(/[\d]+/, convertToCelsius(tempDescTemp));
+                        // const indexDegree = tempDesc.textContent.match(/\u00B0/).index + 1;
+                        // const arraySwitch = tempDesc.textContent.split('');
+                        // arraySwitch[indexDegree] = 'C';
+                        // tempDesc.textContent = arraySwitch.join('');
+                } else {
+                        const tempF = convertToFahrenheit(temp.textContent);
+                        degreeUnit.textContent = 'F';
+                        temp.textContent = tempF;
+
+                        // -----Not Working. Same as above.----//
+                        // const tempDescTemp = tempDesc.textContent.match(/[\d]+/)[0];
+                        // tempDesc.textContent = tempDesc.textContent.replace(/[\d]+/, convertToCelsius(tempDescTemp));
+                        // const indexDegree = tempDesc.textContent.match(/\u00B0/).index + 1;
+                        // const arraySwitch = tempDesc.textContent.split('');
+                        // arraySwitch[indexDegree] = 'F';
+                        // tempDesc.textContent = arraySwitch.join('');
+                }
+        });
+});
